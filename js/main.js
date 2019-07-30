0b676b080c23f59ec91700b5af21dc45c5656248
Cyrus Liang <thecyrusliang@gmail.com>
2019-07-30 16:14:35 -0700

    Update filepaths

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
+function()
+
 $(document).ready(function() {
     console.log('Hello!');
     $('#header').load('https://cyrusliang.github.io/curly-palm-tree/globals/header.html #header-nav');
+    $('section').load('https://cyrusliang.github.io/curly-palm-tree/globals/title.html #title');
     console.log(window.location.pathname);
 });
