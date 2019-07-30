2318fc68b4bf767f4a177f4ea88bcb106164a6b5
Cyrus Liang <thecyrusliang@gmail.com>
2019-07-30 16:25:46 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function loadHeader() {
     $('#header').load('https://cyrusliang.github.io/curly-palm-tree/globals/header.html #header-nav');
-    var github = 'https://github.com/CyrusLiang/curly-palm-tree/blob/master/' + window.location.pathname.substring(16);
+    var github = 'https://github.com/CyrusLiang/curly-palm-tree/blob/master/' + window.location.pathname.substring(17);
     console.log(github);
 }
 function loadTitle() {
