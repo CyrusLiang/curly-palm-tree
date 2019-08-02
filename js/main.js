0975816887e71030b474b65b62023819535d7c3a
Cyrus Liang <thecyrusliang@gmail.com>
2019-08-02 15:48:09 -0700

    Update jQuery

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 function loadHeader() {
     $('#load-header').load('https://cyrusliang.github.io/curly-palm-tree/globals/header.html #header');
-}
-function loadTitle() {
-    $('#load-title').load('https://cyrusliang.github.io/curly-palm-tree/globals/title.html #title');
-}
-
-function doHeader() {
     var github = 'https://github.com/CyrusLiang/curly-palm-tree/';
     var curr = window.location.pathname.substr(17);
     if (curr) {
@@ -13,8 +7,14 @@ function doHeader() {
     }
 
     console.log(github);
-    $('#git-link').attr('href', github);
-    $('#git-link').text('View This Page On GitHub');
+    $('a[href="https://github.com/CyrusLiang/curly-palm-tree"]').attr('href', github);
+    $('a[href="https://github.com/CyrusLiang/curly-palm-tree"]').text('View This Page On GitHub');
+}
+function loadTitle() {
+    $('#load-title').load('https://cyrusliang.github.io/curly-palm-tree/globals/title.html #title');
+}
+
+function doHeader() {
 }
 function doTitle() {
     var title = window.location.pathname;
