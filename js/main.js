451cd2eba55d74eccab617dab325524af31e9401
Cyrus Liang <thecyrusliang@gmail.com>
2019-08-02 15:15:01 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 function loadHeader() {
     $('#header').load('https://cyrusliang.github.io/curly-palm-tree/globals/header.html #header-nav');
+}
+loadHeader();
+function loadTitle() {
+    $('#load-title').load('https://cyrusliang.github.io/curly-palm-tree/globals/title.html #title');
+}
+loadTitle();
+
+function doHeader() {
     var github = 'https://github.com/CyrusLiang/curly-palm-tree/';
     var curr = window.location.pathname.substr(17);
     if (curr) {
@@ -8,10 +16,8 @@ function loadHeader() {
     console.log(github);
     $('.fork a').attr('href', github);
     $('.fork a').text('View This Page On GitHub');
-};
-loadHeader();
-function loadTitle() {
-    $('#load-title').load('https://cyrusliang.github.io/curly-palm-tree/globals/title.html #title');
+}
+function doTitle() {
     var title = window.location.pathname;
     title = title.substr(1, title.length-2);
     if (title.includes('/')) {
@@ -21,8 +27,10 @@ function loadTitle() {
     title = title.replace(/\-/g, ' ');
     console.log(title);
     $('#title h1').text(title);
-};
+}
+
 $(document).ready(function() {
     console.log('Hello!');
-    loadTitle();
+    document.doHeader();
+    document.doTitle();
 });
